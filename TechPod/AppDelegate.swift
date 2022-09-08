1cb9377d8f239e6fb61408f3adcfca746424756f
YuiOgawa <yui20010218@gmail.com>
2022-09-08 11:33:03 +0900

    Initial Commit

---
+++ TechPod/AppDelegate.swift
@@ -0,0 +1,53 @@
+//
+//  AppDelegate.swift
+//  TechPod
+//
+//  Created by Tomoya Tanaka on 2021/07/06.
+//
+
+import UIKit
+import AVFoundation
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		
+		// バックグラウンドで曲を再生
+		let session = AVAudioSession.sharedInstance()
+		do {
+			try session.setCategory(.playback, mode: .default)
+		} catch {
+			// エラー処理
+			fatalError("カテゴリ設定失敗")
+		}
+		
+		// sessionのアクティブ化
+		do {
+			try session.setActive(true)
+		} catch {
+			// audio sessionの有効化が失敗した時
+			fatalError("session有効化失敗")
+		}
+		
+		return true
+	}
+
+	// MARK: UISceneSession Lifecycle
+
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		// Called when a new scene session is being created.
+		// Use this method to select a configuration to create the new scene with.
+		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+	}
+
+	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+		// Called when the user discards a scene session.
+		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+	}
+
+
+}
+
